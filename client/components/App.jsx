0ed9d9f9e6c6268d7f6f879639480bf213b26044
endows <you@example.com>
2015-08-03 01:13:43 +0900

    propsに、文字を入れる

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,29 @@
 var ThemeManager = new MUI.Styles.ThemeManager();
 injectTapEventPlugin();
 
-var { Card,CardHeader,AppBar,FontIcon } = MUI;
+var { Card,CardHeader,AppBar,FontIcon,LeftNav,MenuItem,FloatingActionButton } = MUI;
+menuItems = [
+  { route: 'get-started', text: '自分の投稿したアイディア' },
+  { route: 'customization', text: 'アンケート結果を見る' },
+  { route: 'components', text: 'ご意見' },
+  { type: MenuItem.Types.SUBHEADER, text: 'Resources' },
+  {
+     type: MenuItem.Types.LINK,
+     payload: 'https://github.com/callemall/material-ui',
+     text: 'GitHub'
+  },
+  {
+     text: 'Disabled',
+     disabled: true
+  },
+  {
+     type: MenuItem.Types.LINK,
+     payload: 'https://www.google.com',
+     text: 'Disabled Link',
+     disabled: true
+  },
+];
+
 
 Post = new Mongo.Collection('posts')
 
@@ -19,14 +41,22 @@ var App = React.createClass({
           muiTheme: ThemeManager.getCurrentTheme()
       };
   },
+  _toggle() {
+    this.refs.leftNav.toggle()
+  },
   render() {
+
     return (
         <div>
           <AppBar
             title="iSeed"
-            iconClassNameRight="muidocs-icon-navigation-expand-more" />
+            iconClassNameRight='muidocs-icon-navigation-expand-more' />
             <IdeaList posts={this.data.posts}/>
             <IdeaForm/>
+            <FloatingActionButton onClick={this._toggle} secondary={true}>
+              <FontIcon className="material-icons">menu</FontIcon>
+            </FloatingActionButton>
+            <LeftNav ref="leftNav" docked={false} menuItems={menuItems}/>
         </div>
     );
   }
